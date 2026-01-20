9a5f193b59be2152295bac6f4b1e597d47f8ef82
mftha3022-sketch <mftha3022@gmail.com>
2026-01-20 12:46:32 +0600

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,13 @@
-import { Kalam } from "next/font/google";
+import { Tubahh } from "next/font/google";
 import "./globals.css";
 
-const kalam = Kalam({
+const Tubahh = Tubahh({
   subsets: ["latin"],
   weight: ["300", "400", "700"],
 });
 
 export const metadata = {
-  title: "Happy Birthday!",
+  title: "Happy Birthday Tubuu!",
   description: "Celebrate your day with joy and happiness!",
 };
 
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${kalam.className} antialiased bg-black select-none`}
+        className={`${Tubah.className} antialiased bg-black select-none`}
       >
         {children}
       </body>
